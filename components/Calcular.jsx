c225207c28e436b740307d9a323764deeb532bc3
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-01-28 20:37:51 +0100

    Creo componente TarjetaFondo y termino componente calcular

--- components/Calcular.jsx
+++ components/Calcular.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import Boton from './Boton'
+import TarjetaFondo from './TarjetaFondo'
 
 const Calcular = () => {
   return (
     <section className='sm:py-16 py-6 flex justify-center items-center flex-col relative'>
-      <div/>
-
+      <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full green__gradient'/>
       <div className='w-full flex justify-between items-center md-flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
         <h1 className='font-poppins font-semibold xs:text-[48px] text-[38px] text-white xs:leading-[76.8px] leading-[66.8px] w-full'>Calcula tu rentabilidad <br className='sm:block hidden'/> fácilmente</h1>
         <div className='w-full md:mt-0 mt-6'>
           <p className='font-poppins font-normal text-gray-300 text-[18px] leading-[30.8px] text-left max-w-[450px]'>Obtén una visión precisa de tu rendimiento en fondos indexados con nuestro simulador de inversiones.</p>
         </div>
       </div>
-      <div className='flex flex-wrap sm:justify-start justify-center w-full relative z-[1]'>
-        <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 bg-transparent hover:bg-gradient-to-r from-[#01a78b37] to-[#00fda92d]'>
-          <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>Vanguard U.S. 500 Stock Index Fund EUR Acc</p>
-          <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-2 border-solid border-t-slate-300"/>
-          <Boton styles="mt-10" text={"Calcular"}/>
-        </div>
+      <div className='flex flex-wrap sm:justify-around justify-center w-full relative z-[1]'>
+        <TarjetaFondo nombre="Vanguard U.S. 500 Stock Index Fund" ytd="4,04 %" oneyear="-1,90 %" threeyear="8,92 %" fiveyear="11,34 %" textoBoton={"Calcular Rentabilidad"}/>
+        <TarjetaFondo nombre="Vanguard Emerging Markets Stock Index Fund" ytd="8,28 %" oneyear="-7,26 %" threeyear="0,57 %" fiveyear="1,06 %" textoBoton={"Calcular Rentabilidad"}/>
+        <TarjetaFondo nombre="Nasdaq-100 Index Fund" ytd="7,40 %" oneyear="-19,90 %" threeyear="5,70 %" fiveyear="8,37 %" textoBoton={"Calcular Rentabilidad"}/>
       </div>
+      <Boton styles="mt-10" text="Ver más"/>
     </section>
   )
 }
---
+++ components/TarjetaFondo.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Boton from './Boton'
+
+const TarjetaFondo = ({nombre, ytd, oneyear, threeyear, fiveyear, textoBoton }) => {
+  return (
+    <div className='flex items-center justify-center p-1 bg-gradient-to-r from-[#00FDA8] to-[#01A78A]  rounded-[20px] md:mr-10 sm:mr-5 mr-0 my-5 xs:min-w-[375px] w-full max-w-[375px]'>
+          <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] bg-[#121416] hover:bg-gradient-to-r from-[#01a78b37] to-[#00fda92d] w-full h-full'>
+            <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>{nombre}</p>
+            <div className="w-full pt-6 border-t-2 border-solid border-t-slate-300"/>
+            <div className='flex flex-row justify-around'>
+              <div className="flex flex-col">
+                <p className='font-poppins font-normal text-white text-[15px] xs:text-[18px] leading-[30.8px]'>YTD</p>
+                <p className='font-poppins font-bold text-white text-[15px] xs:text-[18px] leading-[30.8px]'>{ytd}</p>
+              </div>
+              <div className="flex flex-col">
+                <p className='font-poppins font-normal text-white text-[15px] xs:text-[18px] leading-[30.8px]'>1 año</p>
+                <p className='font-poppins font-bold text-white text-[15px] xs:text-[18px] leading-[30.8px]'>{oneyear}</p>
+              </div>
+              <div className="flex flex-col">
+                <p className='font-poppins font-normal text-white text-[15px] xs:text-[18px] leading-[30.8px]'>3 años</p>
+                <p className='font-poppins font-bold text-white text-[15px] xs:text-[18px] leading-[30.8px]'>{threeyear}</p>
+              </div>
+              <div className="flex flex-col">
+                <p className='font-poppins font-normal text-white text-[15px] xs:text-[18px] leading-[30.8px]'>5 años</p>
+                <p className='font-poppins font-bold text-white text-[15px] xs:text-[18px] leading-[30.8px]'>{fiveyear}</p>
+              </div>
+            </div>
+            <Boton styles="mt-10" text={textoBoton}/>
+          </div>
+        </div>
+  )
+}
+
+export default TarjetaFondo
